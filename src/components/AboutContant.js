f39a69abc44afeff8e3c78bf28e09607021e3cc7
abdulbasit200 <asgharabdulbasit.com>
2023-03-24 17:45:40 +0500

    Name of App added

--- src/components/AboutContant.js
+++ src/components/AboutContant.js
@@ -14,10 +14,10 @@ const AboutContant = () => {
         <div className="about-right">
             <div className="image-container">
                 <div className="img-stack top">
-                    <img src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" className="img" alt="Image not found."/>
+                    <img src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" className="img" alt="Not found."/>
                 </div>
                 <div className="img-stack bottom">
-                    <img src="https://images.unsplash.com/photo-1555680510-34daedadbdb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" className="img" alt="Image not found."/>
+                    <img src="https://images.unsplash.com/photo-1555680510-34daedadbdb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" className="img" alt="Not found."/>
                 </div>
             </div>
         </div>
